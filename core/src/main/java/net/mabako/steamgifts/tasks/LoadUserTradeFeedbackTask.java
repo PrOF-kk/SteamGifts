caaf99faadc307bb2bf4e12a313bba6cfba17719
PrOF-kk <valerio.colella.2001@gmail.com>
2025-04-12 22:57:21 +0200

    LoadUserTradeFeedbackTask: Minor refactor

--- core/src/main/java/net/mabako/steamgifts/tasks/LoadUserTradeFeedbackTask.java
+++ core/src/main/java/net/mabako/steamgifts/tasks/LoadUserTradeFeedbackTask.java
@@ -3,6 +3,8 @@ package net.mabako.steamgifts.tasks;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import androidx.annotation.Nullable;
+
 import net.mabako.Constants;
 import net.mabako.steamgifts.data.Comment;
 import net.mabako.steamgifts.data.ICommentHolder;
@@ -36,7 +38,7 @@ public class LoadUserTradeFeedbackTask extends AsyncTask<Void, Void, List<Commen
 
 
     @Override
-    protected List<Comment> doInBackground(Void... params) {
+    protected @Nullable List<Comment> doInBackground(Void... params) {
         Log.d(TAG, "Fetching giveaways for user " + steamID64 + " (" + rating + ") on page " + page);
 
         try {
@@ -57,40 +59,39 @@ public class LoadUserTradeFeedbackTask extends AsyncTask<Void, Void, List<Commen
             Connection.Response response = connection.execute();
             Document document = response.parse();
 
-            if (response.statusCode() == 200) {
-                // FIXME SteamGiftsUserData.extract(fragment.getContext(), document);
-
-                //if (!user.isLoaded())
-                    //foundXsrfToken = Utils.loadUserProfile(user, document);
-
-                user.setPositiveFeedback(Utils.parseInt(document.select(".increment_positive_review_count").text()));
-                user.setNegativeFeedback(Utils.parseInt(document.select(".increment_negative_review_count").text()));
-
-
-                Element rootCommentNode = document.selectFirst(".reviews");
-                if (rootCommentNode != null) {
-                    // Parse all rows of giveaways
-                    ICommentHolder holder = new ICommentHolder() {
-                        private final List<Comment> list = new ArrayList<>();
-
-                        @Override
-                        public List<Comment> getComments() {
-                            return list;
-                        }
-
-                        @Override
-                        public void addComment(Comment comment) {
-                            list.add(comment);
-                        }
-                    };
-                    Utils.loadComments(rootCommentNode, holder, 0, false, true, Comment.Type.TRADE_FEEDBACK);
-                    return holder.getComments();
-                } else
-                    return new ArrayList<>();
-            } else {
+            if (response.statusCode() != 200) {
                 Log.w(TAG, "Got status code " + response.statusCode());
                 return null;
             }
+
+            // FIXME SteamGiftsUserData.extract(fragment.getContext(), document);
+
+            //if (!user.isLoaded())
+            //foundXsrfToken = Utils.loadUserProfile(user, document);
+
+            user.setPositiveFeedback(Utils.parseInt(document.expectFirst(".increment_positive_review_count").text()));
+            user.setNegativeFeedback(Utils.parseInt(document.expectFirst(".increment_negative_review_count").text()));
+
+            Element rootCommentNode = document.selectFirst(".reviews");
+            if (rootCommentNode == null) {
+                return List.of();
+            }
+            // Parse all rows of giveaways
+            ICommentHolder holder = new ICommentHolder() {
+                private final List<Comment> list = new ArrayList<>(rootCommentNode.childrenSize());
+
+                @Override
+                public List<Comment> getComments() {
+                    return list;
+                }
+
+                @Override
+                public void addComment(Comment comment) {
+                    list.add(comment);
+                }
+            };
+            Utils.loadComments(rootCommentNode, holder, 0, false, true, Comment.Type.TRADE_FEEDBACK);
+            return holder.getComments();
         } catch (Exception e) {
             Log.e(TAG, "Error fetching URL", e);
             return null;
@@ -99,8 +100,6 @@ public class LoadUserTradeFeedbackTask extends AsyncTask<Void, Void, List<Commen
 
     @Override
     protected void onPostExecute(List<Comment> result) {
-        super.onPostExecute(result);
-
         if (!user.isFeedbackLoaded() && result != null) {
             user.setFeedbackLoaded(true);
             fragment.onUserUpdated(user);
