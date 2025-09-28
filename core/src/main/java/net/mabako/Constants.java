115f97899b1e98de2c65f5196861377fc09951e9
PrOF-kk <valerio.colella.2001@gmail.com>
2025-09-28 16:43:01 +0200

    Constants: Use class instead of interface

--- core/src/main/java/net/mabako/Constants.java
+++ core/src/main/java/net/mabako/Constants.java
@@ -4,20 +4,22 @@ import android.os.Build;
 
 import java.util.List;
 
-public interface Constants {
+public final class Constants {
+    private Constants() {}
+
     /**
      * Important user roles we want to display. Does not include "Member" or "Game Developer".
      */
-    List<String> IMPORTANT_USER_ROLES = List.of("Administrator", "Ultra Moderator", "Super Moderator", "Senior Moderator", "Junior Moderator");
+    public static final List<String> IMPORTANT_USER_ROLES = List.of("Administrator", "Ultra Moderator", "Super Moderator", "Senior Moderator", "Junior Moderator");
 
     /**
      * User agent to be used for Jsoup connections, which is a generic Chrome build.
      */
-    String JSOUP_USER_AGENT = "Mozilla/5.0 (Linux; Android " + Build.VERSION.RELEASE + ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.0 Safari/537.36";
+    public static final String JSOUP_USER_AGENT = "Mozilla/5.0 (Linux; Android " + Build.VERSION.RELEASE + ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.0 Safari/537.36";
 
     /**
      * <p>Timeout to be used for Jsoup connections in milliseconds.</p>
      * <p>The default is 3 seconds.</p>
      */
-    int JSOUP_TIMEOUT = 10000;
+    public static final int JSOUP_TIMEOUT = 10000;
 }
