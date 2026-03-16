ac11ad3529aeb54e995eee35120d8f8bdf150b45
PrOF-kk <valerio.colella.2001@gmail.com>
2026-03-16 12:20:30 +0100

    Login: Fix endless loading

--- core/src/main/java/net/mabako/steamgifts/activities/LoginActivity.java
+++ core/src/main/java/net/mabako/steamgifts/activities/LoginActivity.java
@@ -9,8 +9,9 @@ import net.mabako.steamgifts.persistentdata.SteamGiftsUserData;
 import org.jsoup.Jsoup;
 
 public class LoginActivity extends SteamLoginActivity {
-    private static final String LOGIN_URL = "https://www.steamgifts.com/?login";
-    private static final String REDIRECTED_URL = "https://www.steamgifts.com/";
+    // SG used to redirect to the Steam login page from https://www.steamgifts.com/?login
+    private static final String LOGIN_URL = "https://steamcommunity.com/openid/login?openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.mode=checkid_setup&openid.return_to=https%3A%2F%2Fwww.steamgifts.com%2F%3Flogin%3D";
+    private static final String REDIRECTED_URL = "https://www.steamgifts.com/?login=";
 
     public LoginActivity() {
         super(REDIRECTED_URL, REDIRECTED_URL);
