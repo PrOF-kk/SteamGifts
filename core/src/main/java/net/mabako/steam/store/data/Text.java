aa3ee1aaa83dd630e75a52c057d0f3b730a2d3c0
PrOF-kk <valerio.colella.2001@gmail.com>
2026-08-16 18:05:15 +0200

    Text: Remove unused loadRemoteImages

--- core/src/main/java/net/mabako/steam/store/data/Text.java
+++ core/src/main/java/net/mabako/steam/store/data/Text.java
@@ -10,29 +10,23 @@ import java.io.Serializable;
 
 public class Text implements IEndlessAdaptable, Serializable {
     @Serial
-    private static final long serialVersionUID = 1675033471669858154L;
+    private static final long serialVersionUID = 8406712980173231148L;
     public static final int VIEW_LAYOUT = R.layout.text_item;
 
     private final String text;
     private final boolean html;
-    private final boolean loadRemoteImages;
 
     @LayoutRes
     private final int layout;
 
     public Text(String text, boolean html) {
-        this(text, html, VIEW_LAYOUT, false);
+        this(text, html, VIEW_LAYOUT);
     }
 
-    public Text(String text, boolean html, boolean loadRemoteImages) {
-        this(text, html, VIEW_LAYOUT, loadRemoteImages);
-    }
-
-    public Text(String text, boolean html, @LayoutRes int layout, boolean loadRemoteImages) {
+    public Text(String text, boolean html, @LayoutRes int layout) {
         this.text = text;
         this.html = html;
         this.layout = layout;
-        this.loadRemoteImages = loadRemoteImages;
     }
 
     public String getText() {
@@ -41,10 +35,6 @@ public class Text implements IEndlessAdaptable, Serializable {
 
     public boolean isHtml() {
         return html;
-    }
-
-    public boolean isLoadRemoteImages() {
-        return loadRemoteImages;
     }
 
     @Override
